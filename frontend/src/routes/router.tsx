eb34ef1ddd906656757cc0cc8f1a47a743a8ff4e
dina <leshlesh1337@gmail.com>
2026-04-27 17:53:56 +0300

    dev ver with cloud db

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,7 +1,7 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { QueryParamProvider } from 'use-query-params';
-import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
-import { AuthProvider } from '../store/providers/AuthProvider.tsx';
+import {createBrowserRouter, Navigate} from 'react-router-dom';
+import {QueryParamProvider} from 'use-query-params';
+import {ReactRouter6Adapter} from 'use-query-params/adapters/react-router-6';
+import {AuthProvider} from '../store/providers/AuthProvider.tsx';
 import PublicLayout from '../ui/layouts/PublicLayout.tsx';
 import LoginPage from '../ui/pages/auth/LoginPage.tsx';
 import RegisterPage from '../ui/pages/auth/RegisterPage.tsx';
@@ -10,49 +10,53 @@ import ItemsPage from '../ui/pages/items/ItemsPage.tsx';
 import ItemDetailsPage from '../ui/pages/items/ItemDetailsPage.tsx';
 import UsersPage from '../ui/pages/users/UsersPage.tsx';
 import ErrorPage404 from '../ui/pages/error/ErrorPage404.tsx';
-import { routes } from './routes.ts';
+import {routes} from './routes.ts';
 import UserProfilePage from '../ui/pages/users/UserProfilePage.tsx';
 
 export const router = createBrowserRouter([
-  {
-    element: (
-      <QueryParamProvider adapter={ReactRouter6Adapter}>
-        <AuthProvider />
-      </QueryParamProvider>
-    ),
-    children: [
-      {
-        path: routes.auth.root,
-        element: <PublicLayout />,
+    {
+        element: (
+            <QueryParamProvider adapter={ReactRouter6Adapter}>
+                <AuthProvider/>
+            </QueryParamProvider>
+        ),
         children: [
-          { path: routes.auth.login, element: <LoginPage /> },
-          { path: routes.auth.register, element: <RegisterPage /> },
+            {
+                path: routes.auth.root,
+                element: <PublicLayout/>,
+                children: [
+                    {path: routes.auth.login, element: <LoginPage/>},
+                    {path: routes.auth.register, element: <RegisterPage/>},
+                ],
+            },
+            {
+                element: <BasicLayout/>,
+                children: [
+                    {
+                        index: true,
+                        element: <Navigate to={routes.items.root} replace/>,
+                    },
+
+                    {
+                        path: routes.items.root,
+                        children: [
+                            {index: true, element: <ItemsPage/>},
+                            {path: routes.items.byId, element: <ItemDetailsPage/>},
+                        ],
+                    },
+                    {
+                        path: routes.users.root,
+                        children: [
+                            {index: true, element: <UsersPage/>},
+                            {path: routes.users.me, element: <UserProfilePage/>},
+                        ],
+                    },
+                ],
+            },
+            {
+                path: '*',
+                element: <ErrorPage404/>,
+            },
         ],
-      },
-      {
-        path: '/',
-        element: <BasicLayout />,
-        children: [
-          {
-            path: routes.items.root,
-            children: [
-              { index: true, element: <ItemsPage /> },
-              { path: routes.items.byId, element: <ItemDetailsPage /> },
-            ],
-          },
-          {
-            path: routes.users.root,
-            children: [
-              { index: true, element: <UsersPage /> },
-              { path: routes.users.me, element: <UserProfilePage /> },
-            ],
-          },
-          {
-            path: '*',
-            element: <ErrorPage404 />,
-          },
-        ],
-      },
-    ],
-  },
+    },
 ]);
